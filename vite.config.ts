8e36cc5b0e5cccf454eb3d5319b448ae5870416d
anpetuwi-sudo <hans.marecek@gmail.com>
2026-02-27 13:32:32 +0100

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,11 @@ import path from "node:path";
 
 export default defineConfig({
   plugins: [react()],
-
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      "@shared": path.resolve(__dirname, "./shared"),
-      "@assets": path.resolve(__dirname, "./attached_assets"),
     },
   },
-
   build: {
     outDir: "dist",
     emptyOutDir: true,
